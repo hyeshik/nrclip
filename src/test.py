6d8bce2204d0c88ad932b71e4c054ad4b0147a8a
Hyeshik Chang <hyeshik@snu.ac.kr>
2012-10-13 13:23:14 +0900

    Remove outdated RandomReadSimulator from rnarryext.

--- src/test.py
+++ src/test.py
@@ -1,17 +1,5 @@
 import rnarryext
-import random
+import numpy as np
 
-d = {}
-for i in range(100):
-    for c in 'ATGC':
-        r = list(c * 172 + 'ATGC-') * 17
-        random.shuffle(r)
-        d[i, c] = ''.join(r)
-
-g = rnarryext.RandomReadCounter(d)
-#print g.simulate('ATTGGCAGAGCGTGCTTAGC', 1000000)
-#print g.simulate('ATGCAAGCTAGTGAAGTCAG', 100)
-r = g.simulate('TTTTGAGGCTTTCTCCCAACGCACAGACTTGTGTAATTCTAACACTAATCCTGTGAAGGGTTGTGGTTGACAGCTGGAGCCTGGGTGACATTCTACA', 1000)
-print r
-print map(rnarryext.shannon_entropy, r)
+print rnarryext.shannon_entropy(np.array([5, 10, 15, 20, 25], 'I'))
 
